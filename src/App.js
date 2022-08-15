ae19ca7c9e888d56239887dbb94bfa49e7156a8a
mARTilda-art <109670365+mARTilda-art@users.noreply.github.com>
2022-08-15 22:05:01 +0200

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by{" "}
-        <a
-          href="https://www.martilda.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          mARTilda
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/mARTilda-art/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by{" "}
+          <a
+            href="https://www.martilda.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            mARTilda
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/mARTilda-art/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
