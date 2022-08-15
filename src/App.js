36c04d95f94daab384c275820a42d06a964d7d5e
mARTilda-art <109670365+mARTilda-art@users.noreply.github.com>
2022-08-15 21:40:24 +0200

    emptied cleared files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Weather App</h1>
+      <p>
+        This website was built by mARTilda and is open-sourced on{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/mARTilda-art/react-weather-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub
         </a>
-      </header>
+      </p>
     </div>
   );
 }
